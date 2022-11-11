0f99b48c7eb51f5af4d94d0d779c2fd9b72ad89a
Pang Ka Ho <pangkaho.14@gmail.com>
2022-11-12 02:36:39 +0800

    added back msg

--- MappingModule/main.c
+++ MappingModule/main.c
@@ -112,6 +112,11 @@ void Mapping_writeHumpData(void)
 //                      Ultrasonic_checkBehind() and UUltrasonic_checkRight() must return false
 void initializeCarPosition(void)
 {
+    // - car need to check ultra sensors. it will need to have the condition where the car is in a corner
+    // - at least 2 of the ultra sensor needs to be blocked. the 2 sensor that are blocked must NOT be opp of one another.
+    // - if above not true, car turn till at least one of the side ultra sensor is blocked. then move forward till front is blocked.
+    // - once done, turn arround so that the front is clear. set car as facing north and starting current position as 00,
+    // - kaho’s mapMaze can commence
 }
 
 // owner            : Kevin
@@ -390,8 +395,9 @@ bool bfs(Vertex *sourceV, Vertex *targetV, Graph *graph)
                 tempVisitedNode1 = tempVisitedNode1->parentVertex;
             }
 
-            //moving the car based on path constructed
-            while( stack1 -> size != 0){
+            // moving the car based on path constructed
+            while (stack1->size != 0)
+            {
                 printf("Size of stack: %d \n", stack1->size);
                 Stack_peak(stack1);
                 driveCarUsingPath(Stack_pop(stack1)); // calling function to drive car.
